367fa9c05e6046914ba1f88db3198b3519dfc3b2
Emilio Axel Sánchez Corona <emilioaxel.sanchezcorona@gmail.com>
2023-08-21 21:31:24 -0600

    UPDATE

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,4 @@
 dirpath=$(pwd);
-alias up >/dev/null 2>&1 && echo "Alias <up> already exists" || echo "alias up='sudo apt update && sudo apt full-upgrade -y'" >> ~/.bash_aliases
 cp -R $dirpath/useful_scripts/.full_scripts/ ~/.full_scripts/
+alias up >/dev/null 2>&1 && echo "Alias <up> already exists" || echo "alias up='sudo apt update && sudo apt full-upgrade -y'" >> ~/.bash_aliases
 alias git-pub >/dev/null 2>&1 && echo "Alias <git-pub> already exists" || echo "alias git-pub='~/.full_scripts/github_publish.sh'" >> ~/.bash_aliases
